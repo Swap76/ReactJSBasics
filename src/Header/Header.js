300aaa32751c00c09f2ecc9cef1837dc4fcc1b95
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-06-13 11:37:00 +0530

    Added Header and Structured the app

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    let app = (
+      <div>
+      <h1 className = "header">Hii Swapnil Here</h1>
+  </div>
+    )
+  return app;
+}
+
+export default Header;
+  
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Resume from './resume'
+import Resume from './Resume/Resume';
+import Header from './Header/Header';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Resume/>, document.getElementById('root')); // This way we can render the element
+// ReactDOM.render(<Resume/>, document.getElementById('root')); // This way we can render the element
+
+ReactDOM.render(<Header/>, document.getElementById('root')); // This way we can render the element
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
